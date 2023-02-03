05d9aeb8373cf80a0ee1ac1c12d8b5e7bcd9f4ee
Cristopher <cristopheralexis.chavez@alumnos.udg.mx>
2023-02-02 18:20:05 -0600

    Mejorando apariencia y posición de elementos

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+const RESPUESTA_AFIRMATIVA = 1;
+const RESPUESTA_NEGATIVA = 0;
 
 let frasePedirInicio = [
   "Algo de",
@@ -10,21 +11,49 @@ let frasePedirFinal = [
   "por favor", "si pudieras",  
 ]
 
-const TOTAL_FRASE_INICIAL = 3;
-const TOTAL_FRASE_FINAL = 2;
+let respuestasNegativas = [
+  "NO", "No señor", "Negativo", "Tu que crees"
+]
 
+let respuestasPositivas = [
+  "Si", "Asi es", "lles sehr", "silbido afirmativo"
+]
 function generarNumeroRandom(valorLimite){
   return Math.round(Math.random() * valorLimite);
 }
 
 function leerTexto(texto){
     let oracion = crearOracion(texto);
-    speechSynthesis.speak(new SpeechSynthesisUtterance(oracion));
+    leerEnVozAlta(oracion);
+    // speechSynthesis.speak(new SpeechSynthesisUtterance(oracion));
+}
+
+function darRespuestaRapida(respuesta){
+  leerEnVozAlta(respuesta);
+  // speechSynthesis.speak(new SpeechSynthesisUtterance(oracion));
+}
+
+function responderSiNo(respuesta){
+  let frase;
+  if(respuesta == RESPUESTA_AFIRMATIVA){
+    frase = respuestasPositivas[generarNumeroRandom(respuestasPositivas.length-1)];
+  }
+  else{
+    frase = respuestasNegativas[generarNumeroRandom(respuestasNegativas.length-1)];
+  }
+
+  leerEnVozAlta(frase);
+}
+
+function leerEnVozAlta(frase){
+  speechSynthesis.speak(new SpeechSynthesisUtterance(frase));
 }
 
 function crearOracion(texto){
-  fraseInicio = generarNumeroRandom(TOTAL_FRASE_INICIAL-1);
-  fraseFinal = generarNumeroRandom(TOTAL_FRASE_FINAL-1);
+  fraseInicio = generarNumeroRandom(frasePedirInicio.length-1);
+  fraseFinal = generarNumeroRandom(frasePedirFinal.length-1);
 
   return frasePedirInicio[fraseInicio] + " " + texto + " " + frasePedirFinal[fraseFinal];
 }
+
+console.log("Elementos frase inicial = " + frasePedirInicio.length);
